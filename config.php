bb4d1b352eef4c01be941959927617ef97fc8611
pbware <contexplore@pbware.de>
2016-08-05 21:00:47 +0200

    Codename hinzugefügt

--- config.php
+++ config.php
@@ -6,6 +6,7 @@
  */
 define('CMS_VERSION', '1.0.0 Alpha 1');
 define('CMS_VERSION_CODE', 100);
+define('CMS_CODENAME', 'Breeze');
 define('CMS_STATUS', 'alpha'); // alpha, beta, rc, final, oldstable, eol
 
 // MySQL
