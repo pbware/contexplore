00cd987a1bba5c2b5e98523bb58a2c13609b35da
pbware <contexplore@pbware.de>
2016-08-05 20:42:39 +0200

    Konfigurationsdatei erstellen

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * ContExplore v1.0
+ * Build 1
+ * Configuration
+ */
+define('CMS_VERSION', '1.0.0 Alpha 1');
+define('CMS_VERSION_CODE', 100);
+define('CMS_STATUS', 'alpha'); // alpha, beta, rc, final, oldstable, eol
+
+// MySQL
+$db_host = '';
+$db_name = '';
+$db_username = '';
+$db_password = '';
+?>
